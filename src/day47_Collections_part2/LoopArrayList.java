4e19830093eb617d01f76c4959a6207f3ef06bf5
AdemTEN <65685868+AdemTEN@users.noreply.github.com>
2020-09-23 10:58:47 +0200

    Collections_part2

--- src/day47_Collections_part2/LoopArrayList.java
+++ src/day47_Collections_part2/LoopArrayList.java
@@ -15,8 +15,30 @@ public class LoopArrayList {
 		nums.add(93);
 		nums.add(5000);
 		nums.add(950);
-		
-		
+
+		// looping using for each loop
+		for (Integer n : nums) {
+			System.out.print(n + "|");
+		}
+
+		System.out.println();
+
+		// looping using for iterator loop
+		for (int i = 0; i < nums.size(); i++) {
+			System.out.print(nums.get(i) + "|");
+		}
+
+		System.out.println();
+
+		// looping using forEach method
+		nums.forEach(n -> System.out.print(n + "|"));
+
+		System.out.println();
+
+		nums.removeIf(n -> n < 500);
+
+		nums.forEach(n -> System.out.print(n + "|"));
+
 
 	}
 
